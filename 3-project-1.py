4a497fbcca64ef5aa376bb9f7787c926173f575d
aliwsh <alifshania222.com>
2024-09-26 21:02:48 +0330

    first python project

---
+++ 3-project-1.py
@@ -0,0 +1,10 @@
+user_number = int( input("choose a number between 1 to 9 : "))
+number = user_number;
+number *= 2;
+number += 8;
+number += user_number;
+number -= 2;
+number /= 3;
+number -= user_number;
+number *= 4;
+print(number)
