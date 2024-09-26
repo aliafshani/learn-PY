4c2ce89eb79ccaea4635dfec95d88b758e9c7a49
aliwsh <alifshania222.com>
2024-09-26 20:36:07 +0330

    first commit

---
+++ first.py
@@ -0,0 +1,40 @@
+# name = "ali"
+# lastName = "afshani"
+
+# my_name = "my name is: 'ali' "
+# my_name = "my name is: 'ali' \nim from iran"
+
+
+# my_name = "my name is: 'ali' \n"
+# my_age = 23;
+# b =""" ich bin ali
+# afshani """
+# print(b)
+
+
+# my_name = "ali afshani"
+# my_age = 30;
+
+# text = f"my name is {my_name} and my age is {my_age}"
+# print(text)
+
+
+
+
+
+text = "im from teheran "
+print(len(text))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
