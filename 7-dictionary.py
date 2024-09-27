e79c351da2c6ee5c9061e2fe50c08cb9ddb2fa24
aliwsh <alifshania222.com>
2024-09-27 20:33:09 +0330

    finish conditions in python

--- 7-dictionary.py
+++ 7-dictionary.py
@@ -1,37 +1,37 @@
-me = {
-    "name" : "alireza",
-    "model" : "c200",
-    "year":2000
-}
+# me = {
+#     "name" : "alireza",
+#     "model" : "c200",
+#     "year":2000
+# }
 
-family = {
-    "vater" : {
-        'name' : "saeed",
-        'age' : 54,
-    },
-    'mutter':{
-        'name': "azadeh",
-        'age' : 48,
-    },
-}
+# family = {
+#     "vater" : {
+#         'name' : "saeed",
+#         'age' : 54,
+#     },
+#     'mutter':{
+#         'name': "azadeh",
+#         'age' : 48,
+#     },
+# }
 
-# x = myDict["name"]
-# x = myDict.get("name")
+# # x = myDict["name"]
+# # x = myDict.get("name")
 
-# x = myDict.keys()
-# x = myDict.values()
-# x = myDict.items()
+# # x = myDict.keys()
+# # x = myDict.values()
+# # x = myDict.items()
 
-# x = "name" in myDict;
+# # x = "name" in myDict;
 
-# me["name"] = "ahmad"
-# me.update({"boobi" : "mimi"})
+# # me["name"] = "ahmad"
+# # me.update({"boobi" : "mimi"})
 
-# me.pop("name")
-# me.popitem()
-# del me["model"]
-# me.clear()
+# # me.pop("name")
+# # me.popitem()
+# # del me["model"]
+# # me.clear()
 
 
 
-print(family['vater']["name"])
+# print(family['vater']["name"])
---
+++ 8-conditions.py
@@ -0,0 +1,8 @@
+a = 33;
+c = 44;
+b = 88;
+
+if a > b and a > c :
+    print("a")
+elif b > a and b > c:
+    print("b")
