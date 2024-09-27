8b9a8fb78604ceada242727255e5f0e345ad836f
aliwsh <alifshania222.com>
2024-09-27 13:54:25 +0330

    lists

---
+++ 4-lists.py
@@ -0,0 +1,9 @@
+# thisList = ["apple" , "banana" , "cherry",True]
+# print(thisList[0:])
+
+# ###########################################################
+
+# doost = ["ali" , "mamad" , "fatemeh" , "avina"]
+# print(doost[:3])
+# print(doost[2:])
+# ############################################################
