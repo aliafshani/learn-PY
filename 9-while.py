d4e502fc4f2788e790f96d3ccbcea0f00a2246dd
aliwsh <alifshania222.com>
2024-09-27 20:42:43 +0330

    finish while  in python

---
+++ 9-while.py
@@ -0,0 +1,13 @@
+# num=1 
+# while num<8:
+#     print(num);
+#     num+=1
+#######################################
+num = 0
+
+while num < 10 :
+    num +=1
+    if num == 6:
+        continue
+    
+    print(num)
