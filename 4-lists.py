4812f03f9498f2b45db296c2ec7688ca81c1cdf7
aliwsh <alifshania222.com>
2024-09-27 14:32:23 +0330

    finish lists

--- 4-lists.py
+++ 4-lists.py
@@ -7,3 +7,22 @@
 # print(doost[:3])
 # print(doost[2:])
 # ############################################################
+# doost = ["ali" , "mamad" , "fatemeh" , "avina"]
+# print(doost)
+# doost[0:]= ["nini" , " nini" , "momo" , "kio"]
+# print(doost)
+# doost = ["ali" , "mamad" , "fatemeh" , "avina"]
+# doost[1:4] =[ "momomo"]
+# print(doost)
+# doost.insert(3 , "kian")
+# print(doost)
+# ###############################################################
+doost = ["ali" , "mamad" , "fatemeh" , "avina"]
+# doost.remove("mamad")
+# doost.sort()
+# print(doost)
+# ##############################################################
+enemy = doost.copy()
+list3 = enemy + doost
+list3.sort()
+print(list3)
