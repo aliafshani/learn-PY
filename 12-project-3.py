d3410695333dc363cdfc628b5e344c786986e079
aliwsh <alifshania222.com>
2024-09-29 09:55:09 +0330

    finish project

--- 12-project-3.py
+++ 12-project-3.py
@@ -1,10 +1,24 @@
-names1 = ["amir" , 'amin' , 'ali' , 'ahmad' , 'reza' , 'akbar' , 'mehdi' , 'milad'] 
-names2 = ["mostafa" , 'amin' , 'kian' , 'nima' , 'jina' , 'akbar' , 'mimi' , 'kir'] 
+# names1 = ["amir" , 'amin' , 'ali' , 'ahmad' , 'reza' , 'akbar' , 'mehdi' , 'milad'] 
+# names2 = ["mostafa" , 'amin' , 'kian' , 'nima' , 'jina' , 'akbar' , 'mimi' , 'kir'] 
 
-empty_list = []
+# empty_list = []
 
-for name1 in names1:
-    for name2 in names2:
-        if name1 == name2:
-            empty_list.append(name1)
-print(empty_list)
+# for name1 in names1:
+#     for name2 in names2:
+#         if name1 == name2:
+#             empty_list.append(name1)
+# print(empty_list)
+
+
+# ###################################################################
+
+name = input("enter your name : ")
+name = name.lower()
+name = name.replace(" " ,"")
+b =[]
+
+
+for item in name:
+    if item not in b:
+        print(f'your name has {name.count(item)}  {item}')
+        b.append(item)
